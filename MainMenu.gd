011d36e4aaac1c85e597e9bab6cbc2d529c259c5
Matt Pietropaoli <mattpietro527@gmail.com>
2022-03-08 00:52:42 -0500

    main menu loop

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
-export var limit : float = -1000
-
+onready var loopTimer = get_node("loopTimer")
 onready var CameraNode2D = $ColorRect/Camera2D
 onready var crawl = $crawl
 onready var orb = $crawl/AnimatedSprite
 onready var StartScreen = get_node("ColorRect/StartScreen")
 
+
 func _process(_delta):
+	
 	var orbPosition = orb.get_global_position()
-#	print(crawlPosition)
-#	print(StartScreen.visible)
-	if orbPosition.y < CameraNode2D.get_global_position().y :
+	
+	if orbPosition.y < CameraNode2D.get_global_position().y or Input.is_action_just_pressed("ui_accept"):
 		StartScreen.visible = true;
-		crawl.visible = 0
-		orb.visible = 0
-		
+		crawl.visible = false
+		orb.visible = false
+		loopTimer.start()
+
+
 
 func _on_StartBtn_pressed():
 	get_tree().change_scene("res://Game.tscn")
@@ -23,3 +25,8 @@ func _on_StartBtn_pressed():
 
 func _on_QuitBtn_pressed():
 	get_tree().quit()
+
+
+func _on_loopTimer_timeout():
+	print("loop timeout")
+	get_tree().reload_current_scene()
--- crawl.gd
+++ crawl.gd
@@ -1,15 +1,15 @@
 extends Label
 
 
-export var speed : float = 9.5
-export var limit : int = 200
+export var speed : float = 13.6
+
+
 
 func _ready():
 	self.set_global_position(Vector2.ZERO)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	var position = self.get_global_position()
 	position.y -= speed * delta
 	position = self.set_global_position(position)
-
